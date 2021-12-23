77fc5e4f8301c346756a5503a5610bf17521d961
THẢO NGUYỄN THỊ MINH <20127412@student.hcmus.edu.vn>
2021-12-23 16:27:19 +0700

    Update cMap.h

--- Main/cMap.h
+++ Main/cMap.h
@@ -26,14 +26,15 @@ public:
 	~cMap() = default;
 	void resetMap(); // reset the map to default with no enemy and no player
 	void printMap(); // print map to screen
-	void hitMap();
-	void redrawMap(); // redraw
-	int draw(cPosition pos, char ** shape, int w, int h); // draw from position pos(x, y), shape(w, h) | 1: ok 0: go out of map, -1: crash
+	void drawMap(); // draw all enemies and player
+	void redrawMap();
+	int draw(cPosition pos, char ** shape, int w, int h); // draw from position pos(x, y) shape(w, h) | 1: ok, 0: go out of map, -1: crash
 	void deleteOldPlayer();
-	void drawPlayer(); // draw player to map, check if crash 
+	void drawPlayer(); // draw player to map, check if crash => kill player
 	void drawEnemies(animal_vehical* enemy); // draw enemy to map
-	void initializeNewState(); // intialize a new map base on level, reset player state		
-	void randomNextState();
+	void randomNextState(); // generate new enemies base on level and push to rows
+	void initializeNewState(); // intialize a new map base on level, reset player state
+							   //    void handleKeyInput(); // receive key input from cGame and call appropriate function
 	void updatePosPlayer(char moving);
 	void bombEffect();
 	void saveGame(string file);
@@ -41,7 +42,9 @@ public:
 	void printBorder();
 	void printCongrats();
 	bool printLevelUp();
+	//check endgame
 	bool isEnd();
+	//check isWin
 	bool isWin();
 	void nextLevel();
 	void printInt(int x, ofstream& outfile);
